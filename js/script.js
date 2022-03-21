82d4bf9a784951d0d9dfa04c7560d4a5aff91a2c
mwaidesu <machariamwai@gmail.com>
2022-03-21 18:14:27 +0300

    Add constructor

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Pizza(size,crust,toppings, total, orderNumber) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.total = total;
+  this.orderNumber = orderNumber;
+}
