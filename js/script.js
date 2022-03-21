aa7ecee54808531d8961d08f8c1e0a424352bfd2
mwaidesu <machariamwai@gmail.com>
2022-03-21 18:18:46 +0300

    Add code to order pizza

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ function Pizza(size,crust,toppings, total, orderNumber) {
   this.total = total;
   this.orderNumber = orderNumber;
 }
+
+$('.order').click(function() {
+  var pizzaSize = $(".size option:selected").val();
+  var pizzaToppings = $(".toppings option:selected").val();
+  var pizzaCrust = $(".crust option:selected").val();
+  var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+  var order = 1;
+  var grandTotal = 0;
+  
+  $("#size").html($(".size option:selected").text() + " - " + pizzaSize);
+  $("#toppings").html($(".toppings option:selected").text() + " - " + pizzaToppings);
+  $("#crust").html($(".crust option:selected").text() + " - " + pizzaCrust);
+  $("#total").html(total);
+})
